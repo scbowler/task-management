413dfb4f022a9fec2a7d1df927472b181c7513ba
Scott Bowler <scott.bowler@learningfuze.com>
2019-01-30 14:07:03 -0800

    Added project auth to delete task collaborator API route

--- controllers/tasks/collaborators/delete.js
+++ controllers/tasks/collaborators/delete.js
@@ -1,44 +1,18 @@
-const { projectUsers, tasks, taskCollaborators, users } = require('../../../db/models');
+const { taskCollaborators } = require('../../../db/models');
 const { errorFlag, sendError, StatusError } = require('../../../helpers/error_handling');
 const { io } = require('../../../services/websocket');
 
 module.exports = async (req, res) => {
-    const { params: { collaborator_id, task_id }, user } = req;
+    const { params: { collaborator_id }, task } = req;
 
     try {
-        const task = await tasks.findByPid(task_id, {
-            attributes: ['id'],
-            include: [
-                {
-                    association: 'project',
-                    attributes: ['id', 'pid']
-                },
-                {
-                    association: 'list',
-                    attributes: ['pid']
-                }
-            ]
-        });
-
-        if(!task) throw new StatusError(422, [], 'Unknown task ID' + errorFlag);
-
-        const isProjectUser = await projectUsers.findOne({
-            attributes: ['id'],
-            where: {
-                projectId: task.project.id,
-                userId: user.id
-            }
-        });
-
-        if(!isProjectUser) throw new StatusError(401, [], 'Not Unauthorized' + errorFlag);
-
         const collaborator = await taskCollaborators.findByPid(collaborator_id);
 
         if(!collaborator) throw new StatusError(422, [], 'Unknown collaborator ID' + errorFlag);
 
         await collaborator.destroy();
 
-        io.of(`/task-${task_id}`).emit('update-collaborators');
+        io.of(`/task-${task.pid}`).emit('update-collaborators');
 
         io.of(`/project-${task.project.pid}`).emit('update-lists', {
             lists: [task.list.pid],
