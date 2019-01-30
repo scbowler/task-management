50cf272377ac0acaf455a82f7bcf0189fdb3561a
Scott Bowler <scott.bowler@learningfuze.com>
2019-01-30 14:23:13 -0800

    Added project auth to toggle lead for project collaborators API route

--- controllers/tasks/collaborators/toggle_lead.js
+++ controllers/tasks/collaborators/toggle_lead.js
@@ -1,37 +1,11 @@
-const { projectUsers, tasks, taskCollaborators, users } = require('../../../db/models');
+const { taskCollaborators } = require('../../../db/models');
 const { errorFlag, sendError, StatusError } = require('../../../helpers/error_handling');
 const { io } = require('../../../services/websocket');
 
 module.exports = async (req, res) => {
-    const { params: { collaborator_id, task_id }, user } = req;
+    const { params: { collaborator_id }, task } = req;
 
     try {
-        const task = await tasks.findByPid(task_id, {
-            attributes: ['id'],
-            include: [
-                {
-                    association: 'project',
-                    attributes: ['id', 'pid']
-                },
-                {
-                    association: 'list',
-                    attributes: ['pid']
-                }
-            ]
-        });
-
-        if(!task) throw new StatusError(422, [], 'Unknown task ID' + errorFlag);
-
-        const isProjectUser = await projectUsers.findOne({
-            attributes: ['id'],
-            where: {
-                projectId: task.project.id,
-                userId: user.id
-            }
-        });
-
-        if(!isProjectUser) throw new StatusError(401, [], 'Not Unauthorized' + errorFlag);
-
         const collaborator = await taskCollaborators.findByPid(collaborator_id);
 
         if(!collaborator) throw new StatusError(422, [], 'Unknown collaborator ID' + errorFlag);
@@ -40,7 +14,7 @@ module.exports = async (req, res) => {
 
         await collaborator.save();
 
-        io.of(`/task-${task_id}`).emit('update-collaborators');
+        io.of(`/task-${task.pid}`).emit('update-collaborators');
 
         io.of(`/project-${task.project.pid}`).emit('update-lists', {
             lists: [task.list.pid],
